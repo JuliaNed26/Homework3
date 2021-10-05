9b716f48a9b15ca22e8ac968ccf89b7d7a3040b0
JuliaNed26 <34402842+JuliaNed26@users.noreply.github.com>
2021-10-05 09:32:35 +0300

    Add files via upload

---
+++ ConverterNextTry.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Converter
+{
+    class Converter
+    {
+        double usd;
+        double eur;
+        public Converter(double _usd,double _eur)
+        {
+            usd = _usd;
+            eur = _eur;
+        }
+        public double GrnToEur(double grnSum)
+        {
+            if (eur == 0)
+            {
+                throw new Exception("eur is 0");
+            }
+            return grnSum / eur;
+        }
+        public double GrnToUsd(double grnSum)
+        {
+            if(usd == 0)
+            {
+                throw new Exception("usd is 0");
+            }
+            return grnSum / usd;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Print usd: ");
+            double usd = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Print eur: ");
+            double eur = Convert.ToDouble(Console.ReadLine());
+            Converter converter = new Converter(usd, eur);
+            Console.Write("Print sum: ");
+            double sum = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Grn to eur: " + converter.GrnToEur(sum));
+            Console.WriteLine("Grn to usd: " + converter.GrnToUsd(sum));
+        }
+    }
+}
